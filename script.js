fdc3b13c5dba14a6bd3dde76d268adeb488d52c1
FaridaAdamuMagaji <faridaadamumagaji@gmail.com>
2025-01-14 01:27:50 -0800

    added card contents

---
+++ script.js
@@ -0,0 +1,13 @@
+const mealList = [
+    {
+        image: './assets/chicken-congee',
+        caption: 'Chicken Congee',
+        steps: 'MARINATING THE CHICKEN In a bowl, add chicken, salt, white pepper, ginger juice and then mix it together well. Set the chicken aside. STEP...',
+        items: 'Chicken - 8 oz Salt - pinch Pepper - pinch Ginger Cordial - 1 tsp Ginger - 1 tsp',
+        button: 'Watch Video',                
+    },
+    {
+        image: './assets/chicken-karaage',
+        caption:
+    }
+]
